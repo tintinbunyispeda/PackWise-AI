10c79bc62cc7eda4ca946792c529f7f07a513101
shantykh2 <shanty.kh2@gmail.com>
2026-06-23 14:33:59 +0700

    fix: explicit vercel nitro preset

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "@core/vite-config";
 
 export default defineConfig({
+  nitro: {
+    preset: "vercel"
+  },
   tanstackStart: {
     server: { entry: "server" },
   },
